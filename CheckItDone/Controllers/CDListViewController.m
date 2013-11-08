27709b9132d749b37a94b621d48b07f309bf412d
Scott Richards <scott.srichards@gmail.com>
2013-11-08 12:12:18 -0800

    Added disclosure to main screen.  Reformatted Task Item to have date at end.

--- CheckItDone/Controllers/CDListViewController.m
+++ CheckItDone/Controllers/CDListViewController.m
@@ -137,6 +137,7 @@
         cell = [[UITableViewCell alloc]
                 initWithStyle:UITableViewCellStyleDefault
                 reuseIdentifier:@"UITableViewCell"];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     }
 
     // Configure the cell...
